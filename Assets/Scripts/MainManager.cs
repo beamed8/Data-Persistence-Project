5b3be360256bfeb22ec2b71c59f81f6771a98176
Kaan <haivron@gmail.com>
2022-06-09 04:08:19 +0300

    saving player name and highscore
    I created a seperate singleton SaveManager class to manage the JSON save file.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -11,21 +12,21 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text bestScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -36,6 +37,9 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+
+        ScoreText.text = $"{SaveManager.Instance.currentPlayerName}'s Score : {m_Points}";
+        bestScoreText.text = "Best Score: " + SaveManager.Instance.playerName + " - " + SaveManager.Instance.playerHighscore;
     }
 
     private void Update()
@@ -65,11 +69,18 @@ public class MainManager : MonoBehaviour
     void AddPoint(int point)
     {
         m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        ScoreText.text = $"{SaveManager.Instance.currentPlayerName}'s Score : {m_Points}";
     }
 
     public void GameOver()
     {
+        if (m_Points > SaveManager.Instance.playerHighscore)
+        {
+            SaveManager.Instance.playerName = SaveManager.Instance.currentPlayerName;
+            SaveManager.Instance.playerHighscore = m_Points;
+            SaveManager.Instance.SaveHighscore();
+        }
+
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance;
+    public string currentPlayerName;
+    public string playerName;
+    public int playerHighscore;
+    public InputField nameInput;
+    public Text currentHighscore;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadHighscore();
+    }
+
+    private void Start()
+    {
+        currentHighscore.text = playerName + " - " + playerHighscore;
+    }
+
+    public void StartGame()
+    {
+        SaveName();
+        SceneManager.LoadScene("main");
+    }
+
+    public void SaveName()
+    {
+        currentPlayerName = nameInput.text;
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string highScoreName;
+        public int highScore;
+    }
+
+    public void SaveHighscore()
+    {
+        SaveData data = new SaveData();
+        data.highScore = playerHighscore;
+        data.highScoreName = playerName;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadHighscore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            playerHighscore = data.highScore;
+            playerName = data.highScoreName;
+
+        }
+    }
+}
